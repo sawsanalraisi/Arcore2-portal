cae9e5073a50493aee28e916d29c28a1dba9667c
sawsanalraisi <44733946+sawsanalraisi@users.noreply.github.com>
2019-10-03 15:35:49 +0400

    1
    1

---
+++ HelloAR/Script/ARController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+#if UNITY_EDITOR
+  using input =GoogleARCore.InstantPreviewInput;
+#endif
+
+
+    public class ARController : MonoBehaviour
+    {
+
+        private List<TrackedPlane> m_NewTrackedPlanes = new List<TrackedPlane>();
+        public GameObject GridPrefab;
+        public GameObject Portal;
+        public GameObject ArCamera;
+        GameObject grid;
+        public bool IsCreate = true;
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+
+
+        public void VisualizePlanes(bool showPlanes)
+        {
+            foreach (GameObject plane in GameObject.FindGameObjectsWithTag("Plane"))
+            {
+                Renderer r = plane.GetComponent<Renderer>();
+                GridVisualiser t = plane.GetComponent<GridVisualiser>();
+                r.enabled = showPlanes;
+                t.enabled = showPlanes;
+            }
+        }
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+
+            if (IsCreate == true)
+            {
+                if (Session.Status != SessionStatus.Tracking)
+                {
+                    return;
+                }
+
+                Session.GetTrackables<TrackedPlane>(m_NewTrackedPlanes, TrackableQueryFilter.New);
+
+                for (int i = 0; i < m_NewTrackedPlanes.Count; i++)
+                {
+                    grid = Instantiate(GridPrefab, Vector3.zero, Quaternion.identity, transform);
+                    grid.GetComponent<GridVisualiser>().Initialize(m_NewTrackedPlanes[i]);
+                }
+
+                Touch touch;
+                if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+                {
+                    return;
+                }
+
+                TrackableHit hit;
+
+                if (Frame.Raycast(touch.position.x, touch.position.y, TrackableHitFlags.PlaneWithinPolygon, out hit))
+                {
+                   // scale portal
+                   //Portal.transform.localScale = new Vector3(1f, 1f, 1f);
+
+
+                    Portal.SetActive(true);
+                    Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                    Portal.transform.position = hit.Pose.position;
+                    Portal.transform.rotation = hit.Pose.rotation;
+                    Vector3 cameraPosition = ArCamera.transform.position;
+                    cameraPosition.y = hit.Pose.position.y;
+                    Portal.transform.LookAt(cameraPosition, Portal.transform.up);
+                   // Portal.transform.parent = anchor.transform;
+                   
+
+                }
+
+                IsCreate = false;
+
+            }
+
+
+            if (IsCreate == false)
+            {
+                foreach (GameObject plane in GameObject.FindGameObjectsWithTag("Plane"))
+                {
+                    Renderer r = plane.GetComponent<Renderer>();
+                    GridVisualiser t = plane.GetComponent<GridVisualiser>();
+                    r.enabled = false;
+                    t.enabled = false;
+                }
+            }
+
+
+        }
+
+    }
---
+++ HelloAR/Script/PotalManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PotalManager : MonoBehaviour
+{
+
+    public GameObject MainCamera;
+    public GameObject []Sponza;
+    private Material[] SponzaMaterials;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        SponzaMaterials = Sponza[0].GetComponent<Renderer>().sharedMaterials;
+        SponzaMaterials = Sponza[1].GetComponent<Renderer>().sharedMaterials;
+        SponzaMaterials = Sponza[2].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[3].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[4].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[5].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[6].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[7].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[8].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[9].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[10].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[11].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[12].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[13].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[14].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[15].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[16].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[17].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[18].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[19].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[20].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[21].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[22].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[23].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[24].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[25].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[26].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[27].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[28].GetComponent<Renderer>().sharedMaterials;
+         SponzaMaterials = Sponza[29].GetComponent<Renderer>().sharedMaterials;
+         
+       
+
+    }
+
+
+    void OnTriggerStay(Collider collider)
+    {
+
+        Vector3 camPositionInPortalSpace = transform.InverseTransformPoint(MainCamera.transform.position);
+
+        if(camPositionInPortalSpace.y<0.5f)
+        {
+            for(int i=0;i<SponzaMaterials.Length;i++)
+            {
+                SponzaMaterials[i].SetInt("_stencilComp",(int)CompareFunction.Always);
+            }
+
+        }
+        else
+        {
+            for (int i = 0; i < SponzaMaterials.Length; i++)
+            {
+                SponzaMaterials[i].SetInt("_stencilComp", (int)CompareFunction.Equal);
+            }
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
