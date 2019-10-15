5934667628336bd36e51facbc8accd6e8f261393
sawsanalraisi <44733946+sawsanalraisi@users.noreply.github.com>
2019-10-15 15:46:34 +0400

    part 3

---
+++ Part 3/HelloAR/Script/BookFuntions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookFuntions : MonoBehaviour
+{
+
+    public GameObject BookCamera;
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "OpenBookTag")
+            {
+            BookCamera.SetActive(true);
+            }
+            
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "OpenBookTag")
+        {
+            BookCamera.SetActive(false);
+        }
+    }
+
+}
---
+++ VR/vr part 3/Scenes/Information.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Information : MonoBehaviour
+{
+
+    public Text ShowInfo;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "BuildInfoTag")
+        {
+            ShowInfo.text = "BuildInfoTag";
+        }
+
+    }
+
+
+
+}
