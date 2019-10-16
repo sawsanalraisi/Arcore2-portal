38431decc95c8bd03da4c3013923c98b808dfac2
sawsanalraisi <44733946+sawsanalraisi@users.noreply.github.com>
2019-10-16 15:41:06 +0400

    vr part 4

---
+++ VR/vr part 4/Scenes/Information.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Information : MonoBehaviour
+{
+
+    public Text ShowInfo;
+
+    public Animator animator;
+  //  bool IsShowBorder;
+    public GameObject SmartcityTxt;
+
+
+    public void Start()
+    {
+       // IsShowBorder = false;
+        SmartcityTxt.SetActive(false);
+        animator = GetComponent<Animator>();
+        animator.enabled = true;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "BuildInfoTag")
+        {
+            animator.SetTrigger("IsShowBorder");
+         //   IsShowBorder = true;
+            SmartcityTxt.SetActive(true);
+            ShowInfo.text = "BuildInfoTag";
+        }
+
+    }
+
+    public void BorderFun()
+    {
+        animator.enabled = true;
+    }
+
+}
